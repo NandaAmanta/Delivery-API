a483a6a19d08f89e637891b0c7c1e59da0a76c41
Nanda Amanta <nansamgr@gmail.com>
2022-07-26 15:02:49 +0800

    Create UserDTO.java

---
+++ test/src/main/java/com/dot/test/dto/UserDTO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dot.test.dto;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ASUS
+ */
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
