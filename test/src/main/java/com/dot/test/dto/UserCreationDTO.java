e80988db44c54ab514f3974132fa554e98464ff5
Nanda Amanta <nansamgr@gmail.com>
2022-07-26 15:02:26 +0800

    Create UserCreationDTO.java
    this is for Handle Client Request Add new User

---
+++ test/src/main/java/com/dot/test/dto/UserCreationDTO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dot.test.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author ASUS
+ */
+@Getter
+@Setter
+public class UserCreationDTO {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
