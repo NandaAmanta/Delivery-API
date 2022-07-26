16cefcb1bb335783ef9818af2652170a590e1e12
Nanda Amanta <nansamgr@gmail.com>
2022-07-26 10:47:45 +0800

    create order and user repository

---
+++ test/src/main/java/com/dot/test/repository/OrderRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dot.test.repository;
+
+import com.dot.test.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ASUS
+ */
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long>{
+    
+}
---
+++ test/src/main/java/com/dot/test/repository/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.dot.test.repository;
+
+import com.dot.test.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ASUS
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
