78f3e22869e9f7baa522c221476a2f4e0700ef3a
Nanda Amanta <nansamgr@gmail.com>
2022-07-27 21:43:09 +0800

    Create CityDTO.java

---
+++ test/src/main/java/com/dot/test/dto/CityDTO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dot.test.dto;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ASUS
+ */
+@Data
+public class CityDTO extends ProvinceDTO {
+
+    private String city_id;
+    private String type;
+    private String city_name;
+    private String postal_code;
+
+}
