7db514daf22eb6de32c42e5a9d013f20ca549ece
Nanda Amanta <nansamgr@gmail.com>
2022-07-28 10:13:24 +0800

    create controller detail city and province

--- test/src/main/java/com/dot/test/controller/DataController.java
+++ test/src/main/java/com/dot/test/controller/DataController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,9 +36,21 @@ public class DataController {
         return ResponseEntity.ok(new ResponseBody(true, "200", "Success Get Provinces", provinces));
     }
 
+    @GetMapping(path = "/provinces/{id}")
+    public ResponseEntity<ResponseBody> getProvince(@PathVariable String id) throws Exception {
+        var province = provinceService.getProvinceDetail(id);
+        return ResponseEntity.ok(new ResponseBody(true, "200", "Success Get Province", province));
+    }
+
     @GetMapping(path = "/cities")
     public ResponseEntity<ResponseBody> getCities(@Nullable @RequestParam String provinceId) throws Exception {
         var cities = cityService.getCities(provinceId);
         return ResponseEntity.ok(new ResponseBody(true, "200", "Success Get cities", cities));
+    }
+
+    @GetMapping(path = "/cities/{id}")
+    public ResponseEntity<ResponseBody> getCity(@PathVariable String id) throws Exception {
+        var city = cityService.getCities(id);
+        return ResponseEntity.ok(new ResponseBody(true, "200", "Success Get city", city));
     }
 }
